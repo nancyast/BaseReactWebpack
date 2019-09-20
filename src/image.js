46e234f6ebb1dfa34c7c120cb22211fb248dcdaf
Nancy Nguyen <41247637+nancyast@users.noreply.github.com>
2019-09-20 10:40:55 +0700

    initial configuration of react application

---
+++ src/image.js
@@ -0,0 +1,5 @@
+import './image.css';
+
+const image = document.createElement('img');
+image.src = 'http://lorempixel.com/300/300/';
+document.body.appendChild(image);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import sum from './math';
+import './image.js';
+
+const total = sum(2, 1);
+console.log(total);
+document.write(total);
